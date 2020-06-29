93679cc99d97056afc464b2941befc8aa64297a8
vyshnavi <vyshnavi.katla526@gmail.com>
2020-06-29 16:24:46 +0530

    File Upload Database

--- models/book.js
+++ models/book.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
-const path = require('path')
-const coverImageBasePath = 'uploads/bookCovers'
-
 
 const bookSchema = new mongoose.Schema({
     title:{
@@ -24,10 +21,14 @@ const bookSchema = new mongoose.Schema({
         required:true,
         default:Date.now
     },
-    coverImageName:{
-         type:String,
+    coverImage:{
+         type:Buffer,
          required:true
     },
+    coverImageType:{
+        type:String,
+        required:true
+   },
     author:{
         type:mongoose.Schema.Types.ObjectId,
         required:true,
@@ -37,10 +38,9 @@ const bookSchema = new mongoose.Schema({
 })
 
 bookSchema.virtual('coverImagePath').get(function() {
-    if(this.coverImageName != null){
-        return path.join('/',coverImageBasePath,this.coverImageName)
+    if(this.coverImage != null && this.coverImageType != null){
+        return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
     }
 })
 
 module.exports = mongoose.model('Book',bookSchema)
-module.exports.coverImageBasePath = coverImageBasePath
